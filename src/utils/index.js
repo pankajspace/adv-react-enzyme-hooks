723294acc380d34623d0d634772cf309289a5799
Pankaj Wakchaure <pankaj.corp@gmail.com>
2021-02-22 17:42:53 +0530

    chech-prop-types installed, extra code removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
-
 import checkPropTypes from "check-prop-types";
-import rootReducer from "../appJotto/reducers";
-import { middlewares } from "../appJotto/configureStore";
 
 export const findElement = (wrapper, attrValue) =>
   wrapper.find(`[data-test='${attrValue}']`);
@@ -14,16 +10,4 @@ export const checkProps = (component, expectedProps) => {
     "prop",
     component.name
   );
-};
-
-// const middlewareEnhancer = applyMiddleware(...middlewares);
-// export const storeFactory = (initialState) => {
-//   return createStore(rootReducer, initialState, middlewareEnhancer);
-// };
-
-export const storeFactory = (initialState) => {
-  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
-    createStore
-  );
-  return createStoreWithMiddleware(rootReducer, initialState);
 };
